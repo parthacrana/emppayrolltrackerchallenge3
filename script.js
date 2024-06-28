1d9842b05a8dfc2186dc90fe94c1539d22d7718d
ParthaRana <parthacrana@gmail.com>
2024-06-27 22:34:12 -0700

    changes before bedtime

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
-// Get a reference to the #add-employees-btn element
+// }===================================================================================================================
+// // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
-  // TODO: Get user input to create and return an array of employee objects
 const collectEmployees = function() {
+  // TODO: Get user input to create and return an array of employee objects
+  const employeesArray = [];
+  let addAnotherEmployee = true;
 
-    const employeesArray = [];
-    let addAnother = true; 
+  while(addAnotherEmployee){
+    const firstName = prompt("Enter employees first name:")
+    const lastName = prompt("Enter employees last name:")
+    let salary = prompt("Enter employees salary:")
+  
+/* Convert salary to a number seperated by commas, must be a number */
+    salary = isNaN(Number(salary)) ? 0 : Number(salary);
 
-
-    const firstName = prompt("Please enter employee's first name")
-    const lastName = prompt("Please enter employee's last name")
-    const salary = prompt("Please enter employee's salary")
-
-  function isInt(x){
-    if (isNaN(x)) {
-      return "Please enter  a number";
-    }
-    return x
-  }
-    // salary = isNaN(Number(salary)) ? 0 : Number(salary);
-
+/* This is to create an employee object -- key value pairs */
     const employee = {
-      firstName: firstName, 
-      lastName: lastName, 
-      salary: salary
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
     };
 
+/* Adds employees details to the array */
     employeesArray.push(employee);
 
-    const continueAdding = confirm("Add another employee?")
+/* Ask user if they want to add another employee */
+    const continueAdding = confirm("Do you want to add another employee?")
     if (!continueAdding){
-      addAnother = false; 
+      addAnotherEmployee = false;
     }
-    return employeesArray;
-
+}
+return employeesArray;
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  // TODO: Calculate and display the average salary
+  const totalSalary = employeesArray.reduce((acc, employee) => acc + employee.salary, 0);
+  const averageSalary = totalSalary / employeesArray.length;
+
+/* Logs average employee salary to console with required message */
+  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is: $${(totalSalary / employeesArray.length).toLocaleString(undefined, {maximumFractionDigits: 2})}`);
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const randomIndex = Math.floor(Math.random() * employeesArray.length);
+  const randomEmployee = employeesArray[randomIndex];
+
+// function toTitleCase(str){                                                           -- was trying to convert first and last names to proper case format
+//   return str.replace(
+//     text => text.chart(0).toUpperCase() + text.substring(1).toLowerCase()
+//   );
+// }
+
+/* Logs random employee to the console with the required message */
+  console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}  , our random drawing winner!`);
+  
+/* Sort employees alphabetically by last name */
+employeesArray.sort((a, b) => a.lastName.localeCompare(b.lastName));
+return employeesArray;
+  
 }
+
 
 /*
   =========================================================================================================================================================
